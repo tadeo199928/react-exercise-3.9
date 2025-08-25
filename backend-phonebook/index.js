a892cfe92320d486d31fd5204cef7a1fbdd58bae
Jose <@bedrocstreaming.com>
2025-08-25 14:49:25 +0100

    Fixing static front

--- backend-phonebook/index.js
+++ backend-phonebook/index.js
@@ -3,10 +3,10 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const path = require('path')
-
 const app = express()
 
 app.use(express.static('dist'))
+
 app.use(cors())
 app.use(express.json()) 
 app.use(morgan('tiny')) 
